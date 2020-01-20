5c07acf50dd8e264fed5ccde5c6ad52986e2ffc2
Luan <luanbonetto42@gmail.com>
2020-01-20 15:12:16 -0300

    criando arquivo de estilização

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import { MyButton } from "../../style/style"
 
 class LoginPage extends Component {
   render() {
     return (
       <div>
-        Oie
+        <MyButton>AAAAA</MyButton>
       </div>
     );
   }

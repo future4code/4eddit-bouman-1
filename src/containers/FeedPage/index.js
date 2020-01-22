ee702719cf670df79cf6f77153478f685010b608
Luan <luanbonetto42@gmail.com>
2020-01-22 14:42:07 -0300

    mapeando os posts

--- src/containers/FeedPage/index.js
+++ src/containers/FeedPage/index.js
@@ -3,12 +3,26 @@ import { connect } from "react-redux";
 import { push } from "connected-react-router";
 import { routes } from "../Router";
 import { getPosts } from "../../actions/lorenzo"
-import { LightBackground, Container, PostBox, ContentBox, Title, Text, ReactionBox, Input } from "../../style/lorenzo"
+import { LightBackground, Container } from "../../style/lorenzo"
+import { PostContainer, PostCard, UserNameBox, UserName, Text, BottonField, CountVote, CountComment, ButtonLight, TextAreaComment } from '../../style/PostPage'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 
 class FeedPage extends Component {
   state = {
     form: {},
   }
+
+  componentDidMount() {
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlphR1dLRWRjZXlaWjlORkxPUGgxIiwiZW1haWwiOiJwZWRyby5kYXJ2YXNAZ21haWwuY29tIiwidXNlcm5hbWUiOiJkYXJ2YXMiLCJpYXQiOjE1NzM1Nzk5MTd9.sKiIIRgiQm2qesnrNmFujNlXF02ytx-IvLKnNFHqXgA"
+    if (token === null) {
+      this.props.goToLoginPage()
+    } else {
+      this.props.getPosts()
+    }
+  } 
 
 
   render() {
@@ -17,24 +31,26 @@ class FeedPage extends Component {
         <Container>
           {this.props.posts.map(post => {
             return (
-              <PostBox>
-                <ContentBox>
-                  <Title>{post.title}</Title>
-                  <br />
-                  {post.username}
-                  <br />
-                  <Text>{post.text}</Text>
-                </ContentBox>
-                <ReactionBox>
-                  <p>
-                    <span onClick={() => this.props.votePost(post.id, 1, this.props.userVoteDirection)}>Up</span>
-                    {post.votesCount}
-                    <span onClick={() => this.props.votePost(post.id, 0, this.props.userVoteDirection)}>Down</span>
-                  </p>
-                  <Input />
-                </ReactionBox>
-              </PostBox>
-            )
+              <PostContainer maxWidth="sm">
+                  <PostCard>
+                      <UserNameBox>
+                          <UserName>{post.username}</UserName>
+                          <Text>{post.text}</Text>
+                      </UserNameBox>
+                      <BottonField>
+                          <FormControlLabel
+                              control={<Checkbox icon={<ArrowUpwardIcon color="default"/>} checkedIcon={<ArrowUpwardIcon color="secondary" />} value="checkedH" />}
+                          />
+                          <CountVote>4343</CountVote>
+                          <FormControlLabel
+                              control={<Checkbox icon={<ArrowDownwardIcon color="default"/>} checkedIcon={<ArrowDownwardIcon color="secondary" />} value="checkedH" />}
+                          />
+                          <CountComment>0 Comentários</CountComment>
+                      </BottonField>
+                  </PostCard>          
+              </PostContainer>
+          
+          )
           })}
         </Container>
       </LightBackground>
@@ -52,4 +68,4 @@ const mapDispatchToProps = dispatch => ({
   getPosts: () => dispatch(getPosts()),
 })
 
-export default FeedPage
+export default connect(mapStateToProps, mapDispatchToProps) (FeedPage)
